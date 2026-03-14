a04d70e4343151df65617faf71f9eea73e00556f
uvemae <uve.mae@gmail.com>
2026-03-14 09:45:02 +0200

    Added Quiz + score logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './styles/variables.css'
 import './styles/App.css'
+import Quiz from "./components/Quiz/Quiz.tsx";
 
 function App() {
     return (
         <div>
             <h1>Statistikaameti Viktoriin</h1>
-            <p>Tekst</p>
+            <Quiz/>
         </div>
     );
 }
